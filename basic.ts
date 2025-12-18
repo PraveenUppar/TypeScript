e8371e3d37d4b6aff93be79d24d1e2038567d734
Praveen Uppar <praveenuppar718@gmail.com>
2025-12-18 13:10:08 +0530

    setup

---
+++ basic.ts
@@ -0,0 +1,7 @@
+// app.ts
+function greet(name: string): string {
+  return `Hello, ${name}! Welcome to TypeScript.`;
+}
+
+let user = "Developer";
+console.log(greet(user));
---
+++ dist/basic.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// app.ts
+function greet(name) {
+    return `Hello, ${name}! Welcome to TypeScript.`;
+}
+let user = "Developer";
+console.log(greet(user));
+//# sourceMappingURL=basic.js.map
