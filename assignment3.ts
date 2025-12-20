d96e99cf723a789afdaa26a42ec7dba2d713213e
Praveen Uppar <praveenuppar718@gmail.com>
2025-12-20 12:04:51 +0530

    assignement 3

--- assignment3.ts
+++ assignment3.ts
@@ -2,8 +2,6 @@
 // TODO: Create interface 'Author' with: id, name, email, joinDate
 // TODO: Create interface 'Post' with: id, title, content, author (Author), tags (string[]), publishedDate
 // TODO: Create at least 2 post objects that follow the interface
-
-// Exercise 2: Optional and readonly properties
 // TODO: Create interface 'Book' with:
 //   - isbn (readonly string)
 //   - title (string)
@@ -11,12 +9,39 @@
 //   - publishedYear (number)
 //   - pages (optional number)
 
-// Exercise 3: Nested interfaces
-// TODO: Create interface 'Company' with: name, industry, address
-// TODO: Create interface 'Employee' with: name, position, salary, company (Company)
-// TODO: Create 2 employee objects
+interface Author {
+  readonly id: number;
+  name: string;
+  email: string;
+  joinDate: number;
+}
+
+interface Post {
+  readonly id: number;
+  title: string;
+  content: string;
+  author: Author;
+  tags: string[];
+  date: number;
+}
+
+const posts: Post = {
+  id: 123,
+  title: "Title",
+  content: "Content",
+  author: {
+    id: 234,
+    name: "pavi",
+    email: "pavi@gmail.com",
+    joinDate: 12 - 12 - 1212,
+  },
+  tags: ["tech", "lifestyle"],
+  date: 12 - 12 - 1212,
+};
 
 // Exercise 4: Type aliases with union types
 // TODO: Create a type 'Status' that can be: "active" | "inactive" | "pending"
 // TODO: Create a type 'UserResponse' that is either a User object or an error string
 // TODO: Create 2 variables with different types from UserResponse
+
+type status = string | string | string;
