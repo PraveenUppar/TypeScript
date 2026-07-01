f260bd65737ce90ae1c669a3e6d60dc0685d8be8
Praveen Uppar <praveenuppar718@gmail.com>
2026-07-01 19:29:11 +0530

    ts day 1

---
+++ Restaurant Recommender/index.ts
@@ -0,0 +1,29 @@
+import restaurants from "./restaurants.js";
+import { type Restaurant } from "./restaurants.js";
+
+// filters
+const price = "2";
+const deliveryTimeMax = 90;
+const maxDistance = 10;
+
+let result: string;
+
+const priceBracket = price.length;
+
+const filteredRestaurants = restaurants.filter((restaurant: Restaurant) => {
+  return (
+    restaurant.priceBracket <= priceBracket &&
+    restaurant.deliveryTimeMinutes <= deliveryTimeMax &&
+    restaurant.distance <= maxDistance
+  );
+});
+
+if (filteredRestaurants.length === 0) {
+  result = "There are no restaurants available right now.";
+} else {
+  result = `We found ${filteredRestaurants.length} restaurants.`;
+}
+
+console.log(result);
+
+// npx tsx "Restaurant Recommender/index.ts"
